a3d7ed7c145cb622ad45151139bee827a58f275f
Gaspar Mari <gaspar.mari@hotmail.com>
2023-04-11 18:31:50 -0300

    agregada base de datos

--- Blog/core/models.py
+++ Blog/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Banda(models.Model):
+    nombre = models.CharField(max_length=20)
+    origen = models.CharField(max_length=20)
+    descripcion = models.TextField(max_length=200)
+    
+    def __str__(self):
+        return f'{self.id} - {self.nombre}'
+    
